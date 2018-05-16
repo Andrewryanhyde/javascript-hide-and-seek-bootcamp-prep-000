2289e1eb86f9da187bd29a29edacee9be8ee60ab
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-16 12:38:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(target, nested) {
-  return document.getElementById(target).querySelector(nested)
+  return document.getElementById(nested)
 }
